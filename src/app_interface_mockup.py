a1adf5f5db6344906d1aa93755d17d15ea107845
Murad <golammurad19@gmail.com>
2026-05-03 22:55:15 +0600

    Fix Streamlit Cloud import error

--- src/app_interface_mockup.py
+++ src/app_interface_mockup.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 try:
     from inverse_consolidation_pinn_uq import run_streamlit_app
 except ModuleNotFoundError:
-    from piml.inverse_consolidation_pinn_uq import run_streamlit_app
+    from src.inverse_consolidation_pinn_uq import run_streamlit_app
 
 
 run_streamlit_app()
